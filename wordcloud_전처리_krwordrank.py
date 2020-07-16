ba50075473ff6c655ba15263c8a6ac55cbaf50a1
951237 <951237@gmail.com>
2020-07-16 23:27:25 +0900

    Doc : Update - 텍스트 파일 읽기 함수 추가

--- wordcloud_전처리_krwordrank.py
+++ wordcloud_전처리_krwordrank.py
@@ -1,9 +1,33 @@
 from krwordrank.hangle import normalize
+'''
+pip install krwordrank
+pip install scipy
+pip install sklearn
+
+참고 : github/lovit/kr-wordrank
+'''
+
+# 텍스트 파일 가져와서 전처리하기
+def get_texts_scores(fname):
+    with open(fname, encoding='utf-8') as f:
+        docs = [doc.lower().replace('\n','').split('\t') for doc in f]
+        docs = [doc for doc in docs if len(doc) == 2]
+
+        if not docs:
+            return [], []
+
+        texts, scores = zip(*docs)
+        return list(texts), list(scores)
+
+# La La Land 라라랜드 텍스트 파일 
+fname = '/Users/mac/Documents/coding_python/python_WordCloud/data/134963.txt'
+texts, scores = get_texts_scores(fname)
+
 
 # text 정제화(불필요한 조사 및 기호들 제거)
     # 자동화 팁 : text는 스크래핑으로 리스트 형식으로 가져오
-texts = ['이것은 예문입니다.', '각자의 데이터를 준비하세요.', '이것은 예문입니다.', '각자의 데이터를 준비하세요.', '이것은 예문입니다.', '각자의 데이터를 준비하세요.', '이것은 예문입니다.', '각자의 데이터를 준비하세요.', '이것은 예문입니다.', '각자의 데이터를 준비하세요.']
-texts = [normalize(texts, english=True, number=True) for text in texts]
+
+texts = [normalize(text, english=True, number=True) for text in texts]
 
 print(texts)
 
@@ -22,4 +46,4 @@ keywords, rank, graph = wordrank_extractor.extract(texts, beta, max_iter)
 
 for word, r in sorted(keywords.items(), key=lambda x:x[1], reverse=True)[:30]:
     print(f'{word} : {r}')
-    print('%8s:\t%4f' % (word, r))
+    # print('%8s:\t%4f' % (word, r))
